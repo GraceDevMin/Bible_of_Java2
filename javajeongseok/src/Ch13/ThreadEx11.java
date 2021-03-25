c1a36cb1c6a7ec646254aa188b2f79a63eeea03c
eunhye <olivgrece@gmail.com>
2021-03-25 10:57:19 +0900

    210325TIL - Thread

--- javajeongseok/src/Ch13/ThreadEx11.java
+++ javajeongseok/src/Ch13/ThreadEx11.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 public class ThreadEx11 {
 	public static void main(String[] args) {
-		
+		ThreadEx11_1 t1 = new ThreadEx11_1("Thread1");
+		ThreadEx11_2 t2 = new ThreadEx11_2("Thread2");
+		t1.start();
+		t2.start();
 	}
 }
 
@@ -34,7 +37,17 @@ class ThreadEx11_2 extends Thread{
 		while(it.hasNext()) {
 			Object obj = it.next();
 			Thread t = (Thread) obj;
+			StackTraceElement[] ste = (StackTraceElement[]) map.get(obj);
 			
+			System.out.println("[" + ++x + "] name : " + t.getName()
+							+ ", group : " + t.getThreadGroup().getName()
+							+ ", daemon : " + t.isDaemon());
+			
+			for(int i=0; i<ste.length; i++) {
+				System.out.println(ste[i]);
+			}
+			
+			System.out.println();
 		}
 	}
 }
